442d396a5f93e39b7bbf63d50e75497a42460e25
cbelling <charlesbellinger@outlook.com>
2022-08-03 13:59:17 -0400

    update

--- src/partials/Plans.jsx
+++ src/partials/Plans.jsx
@@ -4,21 +4,20 @@ function Plans() {
   return (
     <div className="mb-10 mt-10 p-5">
         <div className="max-w-3xl mx-auto text-center pb-2 md:pb-7">
-            <h2 className="h2 mb-4">The Offer</h2>
+            <h2 className="h2 mb-4">Pricing</h2>
           </div>
         <div className="items-center justify-center p-5 flex flex-col">
             <div className="border-2 border-blue-600 rounded drop-shadow-sm p-10 text-center w-full mb-10">
                 <div className="p-3">
                     <h1 className="block font-bold text-xl text-blue-600">Premium</h1>
                     <p className="font-bold text-blue-600">$2,699</p>
-                    <p className="p-1">Guaranteed 700+ Score</p>
-                    <p className="italic p-1">8 Weeks of Test Support</p>
+                    <p className="p-1 italic">Guaranteed 700+ Score in Under 8 Weeks With Less Than 4 Hours Per Week</p>
                     <hr className="mb-2 mt-2"/>
                 </div>
                     <ul role="list" class="list-disc pl-10 space-y-3">
-                        <li></li>
-                        <li className=""></li>
-                        <li>We guarantee that you will score over a 700 (or increase your score by at least 100 points). <span className="text-blue-600">In any case, if you are not happy with your score, we don't get paid.</span></li>
+                        <li>Custom study plan designed and executed to fit your needs</li>
+                        <li>Individual coach / accountability partner to make sure that you stay on track</li>
+                        <li>2x weekly check in's to answers questions and explain new concepts</li>
                     </ul>
             </div>
         </div>
--- src/partials/Testimonials.jsx
+++ src/partials/Testimonials.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import TestimonialImage from '../images/testimonial.jpg';
 
+
 function Testimonials() {
   return (
     <section className="relative">
@@ -22,75 +23,56 @@ function Testimonials() {
           </g>
         </svg>
       </div>
-
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20">
-
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-2 md:pb-4">
-            <h2 className="h2 mb-4">We have helped place students at some of the biggest companies in the world.</h2>
+          <div className="max-w-3xl mx-auto text-center">
+            <h2 className="h2 mb-4">Testimonials</h2>
+            <p className="text-xl text-gray-600">We have worked with students at all different levels who have leveraged their GMAT scores to get into their dream business schools.</p>
           </div>
           {/* Testimonials */}
-          <div className="flex flex-row items-center"> 
-          <div className="max-w-3xl mx-auto mt-20 p-5" data-aos="zoom-y-out">
+          <div className="flex flex-col lg:flex-row items-center"> 
+          <div className="max-w-3xl mx-auto mt-10 p-5" data-aos="zoom-y-out">
             <div className="relative flex items-start border-2 border-gray-200 rounded bg-white">
               {/* Testimonial */}
-              <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0">
-                <div className="absolute top-0 -mt-8 left-1/2 transform -translate-x-1/2">
-                  <svg className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-blue-500" viewBox="0 0 64 64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M37.89 58.338c-2.648-5.63-3.572-10.045-2.774-13.249.8-3.203 8.711-13.383 23.737-30.538l2.135.532c-6.552 10.033-10.532 17.87-11.939 23.515-.583 2.34.22 6.158 2.41 11.457l-13.57 8.283zm-26.963-6.56c-2.648-5.63-3.572-10.046-2.773-13.25.799-3.203 8.71-13.382 23.736-30.538l2.136.533c-6.552 10.032-10.532 17.87-11.94 23.515-.583 2.339.22 6.158 2.41 11.456l-13.57 8.283z" />
-                  </svg>
-                  <img className="relative rounded-full" src={TestimonialImage} width="96" height="96" alt="Testimonial 01" />
-                </div>
+              <div className="text-center px-12 py-8 pt-5 mx-4 md:mx-0">
                 <blockquote className="text-xl font-medium mb-4">
-                  “I cannot thank you all enough. Before working with you, I could not break the 700 mark no matter what I tried. Within 3 weeks I was able to post a 720.“
+                  “I cannot thank you all enough. Before working with you, I could not break the 700 mark no matter what I tried. Within 3 weeks I was able to post a 740.“
                 </blockquote>
-                <cite className="block font-bold text-lg not-italic mb-1">Charlotte R</cite>
+                <cite className="block font-bold text-lg not-italic mb-1">Charlotte R.</cite>
                 <div className="text-gray-600 flex flex-col">
-                  <a className="text-blue-600 hover:underline" href="#0">Premium Member</a>
-                  <span>Insurance Analyst</span> 
+                  <a className="text-blue-600 hover:underline" href="#0">Harvard Business School</a>
+                  <span>Class of '24</span> 
                 </div>
               </div>
             </div>
           </div>
-          <div className="max-w-3xl mx-auto mt-20 p-5" data-aos="zoom-y-out">
+          <div className="max-w-3xl mx-auto mt-10 p-5" data-aos="zoom-y-out">
             <div className="relative flex items-start border-2 border-gray-200 rounded bg-white">
               {/* Testimonial */}
-              <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0">
-                <div className="absolute top-0 -mt-8 left-1/2 transform -translate-x-1/2">
-                  <svg className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-blue-500" viewBox="0 0 64 64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M37.89 58.338c-2.648-5.63-3.572-10.045-2.774-13.249.8-3.203 8.711-13.383 23.737-30.538l2.135.532c-6.552 10.033-10.532 17.87-11.939 23.515-.583 2.34.22 6.158 2.41 11.457l-13.57 8.283zm-26.963-6.56c-2.648-5.63-3.572-10.046-2.773-13.25.799-3.203 8.71-13.382 23.736-30.538l2.136.533c-6.552 10.032-10.532 17.87-11.94 23.515-.583 2.339.22 6.158 2.41 11.456l-13.57 8.283z" />
-                  </svg>
-                  <img className="relative rounded-full" src={TestimonialImage} width="96" height="96" alt="Testimonial 01" />
-                </div>
+              <div className="text-center px-12 py-8 pt-5 mx-4 md:mx-0">
                 <blockquote className="text-xl font-medium mb-4">
-                  “ I cannot thank Austin and the rest of Bellinger & Co enough. They helped me go from nothing to mastering all necessary skills to succeed in my interviews. With their help, I was able to land my dream position in under a month!“
+                  “The help that I recieved was absolutely invaluable. I had limited time to prep and the folks at Bellinger & Co worked around my schedule to accomodate me.“
                 </blockquote>
-                <cite className="block font-bold text-lg not-italic mb-1">Charlotte R</cite>
+                <cite className="block font-bold text-lg not-italic mb-1">Evan B.</cite>
                 <div className="text-gray-600 flex flex-col">
-                  <a className="text-blue-600 hover:underline" href="#0">Premium Member</a>
-                  <span>Insurance Analyst</span> 
+                  <a className="text-blue-600 hover:underline" href="#0">Ross School of Business</a>
+                  <span>Class of '23</span> 
                 </div>
               </div>
             </div>
           </div>
-          <div className="max-w-3xl mx-auto mt-20 p-5" data-aos="zoom-y-out">
+          <div className="max-w-3xl mx-auto mt-10 p-5" data-aos="zoom-y-out">
             <div className="relative flex items-start border-2 border-gray-200 rounded bg-white">
               {/* Testimonial */}
-              <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0">
-                <div className="absolute top-0 -mt-8 left-1/2 transform -translate-x-1/2">
-                  <svg className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-blue-500" viewBox="0 0 64 64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M37.89 58.338c-2.648-5.63-3.572-10.045-2.774-13.249.8-3.203 8.711-13.383 23.737-30.538l2.135.532c-6.552 10.033-10.532 17.87-11.939 23.515-.583 2.34.22 6.158 2.41 11.457l-13.57 8.283zm-26.963-6.56c-2.648-5.63-3.572-10.046-2.773-13.25.799-3.203 8.71-13.382 23.736-30.538l2.136.533c-6.552 10.032-10.532 17.87-11.94 23.515-.583 2.339.22 6.158 2.41 11.456l-13.57 8.283z" />
-                  </svg>
-                  <img className="relative rounded-full" src={TestimonialImage} width="96" height="96" alt="Testimonial 01" />
-                </div>
+              <div className="text-center px-12 py-8 pt-5 mx-4 md:mx-0">
                 <blockquote className="text-xl font-medium mb-4">
-                  “ I cannot thank Austin and the rest of Bellinger & Co enough. They helped me go from nothing to mastering all necessary skills to succeed in my interviews. With their help, I was able to land my dream position in under a month!“
+                  “I came to B&C with only 5 weeks to prepare for the GMAT and with thier help I was able to score a 710 on my first try!“
                 </blockquote>
-                <cite className="block font-bold text-lg not-italic mb-1">Charlotte R</cite>
+                <cite className="block font-bold text-lg not-italic mb-1">Anthony L.</cite>
                 <div className="text-gray-600 flex flex-col">
-                  <a className="text-blue-600 hover:underline" href="#0">Premium Member</a>
-                  <span>Insurance Analyst</span> 
+                  <a className="text-blue-600 hover:underline" href="#0">Haas School of Business</a>
+                  <span>Class of '23</span> 
                 </div>
               </div>
             </div>
